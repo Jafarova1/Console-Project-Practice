062403d0777b5ea660b3bda28bf7a9fa1c41f689
jafarova1 <gultajvj@code.edu.az>
2022-12-03 11:59:57 -0800

    added file

---
+++ Console project practice/Console project practice/Controllers/LibraryController.cs
@@ -0,0 +1,104 @@
+﻿using Domain.Entities;
+using ServiceLayer.Helpers;
+using ServiceLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_project_practice.Controllers
+{
+    public class LibraryController
+    {
+        public readonly LibraryService _libraryService;
+        public LibraryController()
+        {
+            _libraryService=new LibraryService();
+        }
+        public void Create()
+        {
+            try
+            {
+                ConsoleColor.DarkMagenta.writeConsole("Add library name");
+
+                string name = Console.ReadLine();
+
+                ConsoleColor.DarkMagenta.writeConsole("Add library seat count");
+
+            SeatCount: string seatCountStr = Console.ReadLine();
+
+                int seatCount;
+
+                bool isParseSeatCount = int.TryParse(seatCountStr, out seatCount);
+
+
+                if (isParseSeatCount)
+                {
+                    Library library = new()
+                    {
+                        Name = name,
+                        SeatCount = seatCount,
+                    };
+
+                    
+                    var result = _libraryService.Create(library);
+                    ConsoleColor.Green.writeConsole($"{result.Name} {result.SeatCount}");
+                }
+                else
+                {
+                    ConsoleColor.Red.writeConsole("please add correct seat count");
+                    goto SeatCount;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ConsoleColor.Red.writeConsole(ex.Message); 
+            }
+           
+            
+        }
+
+        public void GetById()
+        {
+            try
+            {
+
+                ConsoleColor.DarkMagenta.writeConsole("Add library id:");
+
+              Id: string idstr = Console.ReadLine();
+
+                int id;
+
+                bool isParseId=int.TryParse(idstr, out id);
+
+                if (isParseId)
+                {
+                    var result=_libraryService.GetById(id);
+                    if(result is null)
+                    {
+                        ConsoleColor.Red.writeConsole("Notfound,please try again");
+                        goto Id;
+                    }
+                    ConsoleColor.Green.writeConsole($"{result.Name} {result.SeatCount}");
+
+                }
+                else
+                {
+                    ConsoleColor.Red.writeConsole("please add correct Id");
+                    goto Id;
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                ConsoleColor.Red.writeConsole(ex.Message;
+
+            }
+        }
+    }
+}
---
+++ Console project practice/Console project practice/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using Console_project_practice.Controllers;
+using ServiceLayer.Helpers;
+using System;
+
+
+LibraryController libraryController=new ();
+
+while (true)
+{
+   
+
+    ConsoleColor.Blue.writeConsole("Select one option:");
+
+    
+    ConsoleColor.Blue.writeConsole("Library options: 1-create,2-get by id, 3-Delete");
+   SelectOption: string option =Console.ReadLine();
+
+    int selectedoption;
+
+    bool IsparseOption=int.TryParse(option, out selectedoption);
+
+    if (IsparseOption)
+    {
+        switch (selectedoption)
+        {
+            case 1:
+                libraryController.Create();
+                break;
+            case 2:
+                libraryController.GetById();
+                break;
+            case 3:
+                Console.WriteLine("Delete");
+                break;
+            default:
+                Console.WriteLine("Select again true option");
+                goto SelectOption;
+              
+        }
+    }
+    else
+    {
+        ConsoleColor.Red.writeConsole("Select again true option");
+        goto SelectOption;
+        
+    }
+}
